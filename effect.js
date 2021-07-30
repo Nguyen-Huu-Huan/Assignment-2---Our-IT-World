07beae1684ea0714b4036d95c5ad5f8b2db09194
Huan Nguyen <s3887111@rmit.edu.vn>
2021-07-31 05:28:08 +0700

    Fix bug and add menu slider

--- effect.js
+++ effect.js
@@ -4,7 +4,7 @@ function underline(target) {
   var line = document.querySelector(target_reformat);
   var anchor = document.querySelector(target_anchor);
   anchor.style.color = "#4568f5";
-  line.style.display = "block";
+  line.style.visibility = "visible";
   anchor.style.fontWeight = "bold";
   line.style.animation = "line_extend 0.2s";
   line.style.transform = "scaleX(2)";
@@ -14,19 +14,28 @@ function no_underline(target) {
   var target_anchor = '.a_'+target;
   var line = document.querySelector(target_reformat);
   var anchor = document.querySelector(target_anchor);
-  line.style.animation = "line_shrink 0.2s";
+  line.style.animation = "line_shrink 0.3s";
   line.style.transform = "scaleX(0)";
   anchor.style.color = "purple";
   anchor.style.fontWeight = "normal";
 }
 var all_content = document.querySelectorAll('.aboutus-content>div');
+var all_focus = document.querySelectorAll('.focus');
 function active(target) {
   all_content.forEach((item, i) => {
     var content_children = "#"+item.id+">h2";
-    document.querySelector(content_children).style.background = "white";
+    document.querySelector(content_children).style.color = "black";
   });
+  all_focus.forEach((item, i) => {
+    var focus_children = "."+item.class;
+    item.style.visibility = "hidden";
+  });
+
   var target_reformat = '#'+target+">h2";
+  var target_focus = '.focus-icon-'+target;
   var highlight_element = document.querySelector(target_reformat);
-  highlight_element.style.background = "#03fcbe";
+  var focus = document.querySelector(target_focus);
+  highlight_element.style.color = "red";
   highlight_element.style.transition = "0.5s ease-in-out";
+  focus.style.visibility = "visible";
 }
